a8a0c6662ea6f25b556e7ceb13a2b27cace9d29e
makoto kawahara <makoto.kawahara@justincase-tech.com>
2023-04-03 20:50:06 +0900

    add: font-size

--- assets/js/applyColors.js
+++ assets/js/applyColors.js
@@ -13,12 +13,23 @@ async function loadColors() {
   function applyColors(data) {
     const primaryColor = data.global.main.primaryColor.value;
     const secondaryColor = data.global.main.secondaryColor.value;
+    const fs1 = data.global.fontsize.fs1.value;
+    const fs2 = data.global.fontsize.fs2.value;
+    const fs3 = data.global.fontsize.fs3.value;
+    const fs4 = data.global.fontsize.fs4.value;
+    const fontSizeBase = data.global.fontsize.fontSizeBase.value;
+    const fontSizeParagraph = data.global.fontsize.fontSizeParagraph.value;
     
     // CSSを動的に追加
     const style = document.createElement('style');
     style.innerHTML = `
       .primary { color: ${primaryColor}; }
       .secondary { color: ${secondaryColor}; }
+      .fs1 { font-size: ${fs1}; }
+      .fs2 { font-size: ${fs2}; }
+      .fs3 { font-size: ${fs3}; }
+      .fs4 { font-size: ${fs4}; }
+      .fontSizeParagraph { font-size: ${fontSizeParagraph}; }
     `;
     document.head.appendChild(style);
   }
